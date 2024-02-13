818b4ca4b8c86bf0feeb974d0e097424c92df9b0
James <lim.en.hui.james@gmail.com>
2024-02-13 15:31:38 +0000

    Added button

---
+++ frontend/src/app/components/add-button.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react";
+
+const AddButton = () => {
+  return (
+    <Button aria-label="Queue New User">
+      Queue New User
+    </Button>
+  );
+}
+
+export default AddButton;
--- frontend/src/app/queue/page.tsx
+++ frontend/src/app/queue/page.tsx
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
 import CardQueue from "../components/card-queue";
+import AddButton from "../components/add-button";
 
 const QueuePage = () => {
-  return <CardQueue />;
+  return (
+    <div>
+      <h1 className="text-2xl text-center my-4">Current Queue</h1>
+      <CardQueue />
+      <AddButton />
+    </div>
+  );
 }
 
 export default QueuePage;
