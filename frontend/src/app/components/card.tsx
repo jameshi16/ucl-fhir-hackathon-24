182529a5e52cb2c20364aa8cdb42b29d2841f93c
James <lim.en.hui.james@gmail.com>
2024-02-13 15:05:05 +0000

    Comment out background color

--- frontend/src/app/components/card.tsx
+++ frontend/src/app/components/card.tsx
@@ -12,21 +12,23 @@ type CardProps = {
 
 const Card = ({ data, position, provided }: CardProps) => {
   return (
-    <div style={{
-      boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-      padding: '1em',
-      margin: '1em 0',
-      borderRadius: '4px'
-    }}
-      ref={provided.innerRef}
+    <div ref={provided.innerRef}
       {...provided.draggableProps}
       {...provided.dragHandleProps}>
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <b>{data.name}</b>
-        <span>Position: {position + 1}</span> {/* plus 1 because index is 0 based */}
+      <div style={{
+        boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+        padding: '1em',
+        margin: '1em 0',
+        borderRadius: '4px',
+        backgroundColor: 'grey',
+      }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <b>{data.name}</b>
+          <span>Position: {position + 1}</span> {/* plus 1 because index is 0 based */}
+        </div>
+        <p>{data.conditions.join(', ')}</p>
       </div>
-      <p>{data.conditions.join(', ')}</p>
-    </div >
+    </div>
   );
 }
 
--- frontend/src/app/queue/page.tsx
+++ frontend/src/app/queue/page.tsx
@@ -1,62 +1,8 @@
-'use client';
-
 import React, { useState } from "react";
-import { DragDropContext, Droppable, Draggable, DropResult } from "react-beautiful-dnd";
-import Card from '@/app/components/card';
+import CardQueue from "../components/card-queue";
 
-const reorder = <T,>(list: T[], startIndex: number, endIndex: number): T[] => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-  return result;
-};
-
-const CardQueue = () => {
-  const [items, setItems] = useState([{
-    id: '1',
-    name: 'Hellen Smith',
-    conditions: ['Diabetes', 'Asthma'],
-  }, {
-    id: '2',
-    name: 'John Doe',
-    conditions: ['Chronic Depression']
-  }, {
-    id: '3',
-    name: 'Jane Doe',
-    conditions: ['Thingy']
-  }]);
-
-  const onDragEnd = (result: DropResult) => {
-    if (!result.destination) return;
-
-    const { source, destination } = result;
-    setItems(reorder(items, source.index, destination.index));
-  }
-
-  return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="droppable">
-        {(provided) => (
-          <div
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-          >
-            {items.map((item, index) => (
-              <Draggable key={item.id} draggableId={item.id} index={index}>
-                {(provided) => (
-                  <Card
-                    data={item}
-                    position={index}
-                    provided={provided} />
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
-  );
+const QueuePage = () => {
+  return <CardQueue />;
 }
 
-export default CardQueue;
+export default QueuePage;
